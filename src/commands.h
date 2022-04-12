40abfd96d73ed2d8e643ee6b8fd59baa25a9a8b7
Daniel Meier <35268604+ganjoe@users.noreply.github.com>
2022-04-12 02:19:00 +0200

    cmd setter ergänzt

--- src/commands.h
+++ src/commands.h
@@ -12,17 +12,17 @@ CmdMessenger cmdMessenger = CmdMessenger(BTUART);
 void attachCommandCallbacks()
 {
   // Attach callback methods
-  cmdMessenger.attach(khelp, help);
-  cmdMessenger.attach(kPeriodicReport, setPeriodicReport);
-  cmdMessenger.attach(ksetAvgSamples, setAvgSamples);
-  cmdMessenger.attach(kPeriodicReport, setPeriodicReport);
-  cmdMessenger.attach(ksetCellLowVolts, setCellLowVolts);
-  cmdMessenger.attach(ksetCellHighVolts, setCellHighVolts);
-  cmdMessenger.attach(ksetCellmaxDiff, setCellmaxDiff);
-  cmdMessenger.attach(kSaveGame, SaveGame);
-  cmdMessenger.attach(kLoadGame, cmdLoadGame);
-  cmdMessenger.attach(kShowConfig,ShowConfig);
-  cmdMessenger.attach(kLoadDefaults,LoadDefaults);
+  cmdMessenger.attach(khelp,            help);
+  cmdMessenger.attach(kPeriodicReport,  cmdSetPeriodicReport);
+  cmdMessenger.attach(ksetAvgSamples,   cmdSetAvgSamples);
+  cmdMessenger.attach(ksetLowHighDiff,  cmdSetLowHighDiff);
+  cmdMessenger.attach(ksetTempLowHigh,  cmdSetTempLowHigh);
+  cmdMessenger.attach(ksetAdcScaler,    cmdSetAdcScaler);
+  cmdMessenger.attach(kSaveGame,        cmdSaveGame);
+  cmdMessenger.attach(kLoadGame,        cmdLoadGame);
+  cmdMessenger.attach(kLoadDefaults,    cmdLoadDefaults);
+  cmdMessenger.attach(kShowConfig,      cmdShowConfig);
+
 }
 
 void help()
@@ -35,23 +35,53 @@ void help()
   Serial.println(F(" 0;                      - diese Ausgabe")); 
   Serial.println(F(" 1,<setPeriodicReport>;  - Live-Daten Spam, 0..10: updates/sec"));
   Serial.println(F(" 2,<setAvgSamples>;      - samples für mittelwerte pro kanal, 1..4096")); 
-  Serial.println(F(" 3,<setCellLowVolts>;    - UVP Einzelzelle[mV] ")); 
-  Serial.println(F(" 4,<setCellHighVolts>;   - OVP Einzelzelle[mV]")); 
-  Serial.println(F(" 5,<setCellmaxDiff>;     - max. Diff. Batt[mV]"));
+  Serial.println(F(" 3,<setLowHighDiff>;     - Limits für jede Zelle[mV].. ")); 
+  Serial.println(F("    ...Diff ist die Maximale Abweichung im Akku")); 
+  Serial.println(F(" 4,<setTempLowHigh>;     - Limits für Zelltemperatur "));
+  Serial.println(F(" 5,<setAdcScaler>;       - Faktor für ADC[bits]->mV... "));
+  Serial.println(F("    ...<Zelle 0-7><(float)Faktor>... "));  
   Serial.println(F("#######################################################"));
-  Serial.println(F("# Savegame 0 sind die Reset-Defaults !!               #"));
+  Serial.println(F("# Savegame 0 wird stets nach Reset geladen            #"));
   Serial.println(F("#######################################################"));
   Serial.println(F(" 6,<Save Config>          - Slot 0-4 Parameter speichern")); 
-  Serial.println(F(" 7,<Load Config>          - Slot 0-4, Parameter laden")); 
-  Serial.println(F(" 8,<Load Defaults>       - Factory defaults laden"));
+  Serial.println(F(" 7,<Load Config>          - Slo1t 0-4, Parameter laden")); 
+  Serial.println(F(" 8,<Load Defaults>        - Factory defaults laden"));
   Serial.println(F(" 9,<Show Config>          - Slot 0-4"));
  
 }
-void setCellLowVolts()  {  Serial.println(F("setCellLowVolts:")); }
-void setCellHighVolts() {  Serial.println(F("setCellHighVolts:")); }
-void setCellmaxDiff()   {  Serial.println(F("setCellmaxDiff:")); }
+void cmdSetLowHighDiff()   
+{  
+  akku.cell_min_voltage = cmdMessenger.readInt16Arg();;
+  akku.cell_max_voltage =cmdMessenger.readInt16Arg();
+  akku.cell_max_diff = cmdMessenger.readInt16Arg();
+  Serial.println(F("setLowHighDiff:")); 
+  Serial.print(F("Lower Limit:"));  Serial.println(akku.cell_min_voltage);
+  Serial.print(F("Max Diff:"));     Serial.println(akku.cell_max_diff);
+  Serial.print(F("Upper Limit:"));  Serial.println(akku.cell_max_voltage);
+}
+void cmdSetTempLowHigh()   
+{  
+  akku.cell_min_temperature = cmdMessenger.readInt16Arg();
+  akku.cell_max_temperature =cmdMessenger.readInt16Arg();
 
-void setAvgSamples()
+  Serial.println(F("SetTempLowHigh:")); 
+  Serial.print(F("Lower Temperature Limit:"));  Serial.println(akku.cell_min_temperature);
+  Serial.print(F("Upper Temperature Limit:"));  Serial.println(akku.cell_max_temperature);
+}
+void cmdSetAdcScaler()     
+{
+  int pos = cmdMessenger.readInt16Arg();
+  float faktor = cmdMessenger.readFloatArg();
+  akku.resFaktor[pos] = faktor;
+  for (size_t i = 0; i < akku.cellcount; i++)
+  {
+    Serial.println(akku.resFaktor[i],6);    
+  }
+  Serial.println(F("...done")); 
+  
+}
+
+void cmdSetAvgSamples()
 {
   int temp = cmdMessenger.readInt16Arg();
   if (temp==0)
@@ -66,7 +96,7 @@ void setAvgSamples()
     Serial.println(livedata.samplecount, DEC);
   }
 };
-void setPeriodicReport()
+void cmdSetPeriodicReport()
 { 
 dlog.flag_PeriodicReportEnable = cmdMessenger.readInt16Arg();
 Serial.print(F("flag_PeriodicReportEnable:"));
@@ -74,7 +104,7 @@ Serial.println(dlog.flag_PeriodicReportEnable, DEC);
 };
 
 /*------Storage------------*/
-void SaveGame()
+void cmdSaveGame()
 {
   
   int pos = 0;
@@ -105,13 +135,13 @@ void SaveGame()
   
 }
 void cmdLoadGame(){LoadGame(cmdMessenger.readInt16Arg());}
-void LoadDefaults()
+void cmdLoadDefaults()
 {
   confgenLoadDefaults(&akku);
   Serial.print(F("Load Defaults from Progmem:"));
-  ShowConfig();
+  cmdShowConfig();
 }
-void ShowConfig()
+void cmdShowConfig()
 {
   confgenReport(&akku);
 }
